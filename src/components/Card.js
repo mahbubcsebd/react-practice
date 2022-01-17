f704dce9e1fbf56b7f45cf41bf734d0321b6a51c
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2022-01-17 22:22:31 +0600

    destructuring object

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,10 @@
-let title = 'My Card';
-let desc = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolores odit aut iste quam praesentium!';
-
 // Component
 function Card(props){
+  // Destructuring Object
+  const {title,desc} = props;
   return <div className="card">
-            <h1 className="card-title" id="myId">{props.title}</h1>
-            <p className='card-desc'>{props.desc}</p>
+            <h1 className="card-title" id="myId">{title}</h1>
+            <p className='card-desc'>{desc}</p>
         </div>
 }
 
